2eebff7769dbea0abbe797a52d05f2f38352d404
Rob Hand <146272+sinon@users.noreply.github.com>
2025-02-10 20:52:48 +0000

    WIP:  Handle APIVersions requests #pv1

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,16 @@ struct Message {
     request_api_key: u16,
     request_api_version: u16,
     correlation_id: u32,
+    error_code: u16,
 }
 
 impl Message {
     async fn read_message(stream: &mut TcpStream, len: u32) -> Self {
+        /*Request Header v0 => request_api_key request_api_version correlation_id
+        request_api_key => INT16
+        request_api_version => INT16
+        correlation_id => INT32 */
+
         // Read the message using the previous len
         let mut msg_buf = vec![0u8; len as usize];
         stream.read_exact(msg_buf.as_mut_slice()).unwrap();
@@ -26,6 +32,24 @@ impl Message {
             request_api_key,
             request_api_version,
             correlation_id,
+            error_code: 0,
+        }
+    }
+
+    fn create_api_versions_response(
+        correlation_id: u32,
+        request_api_key: u16,
+        request_api_version: u16,
+    ) -> Self {
+        let error_code = match request_api_version {
+            4..=18 => 0,
+            _ => 35,
+        };
+        Message {
+            correlation_id,
+            error_code,
+            request_api_key,
+            request_api_version,
         }
     }
 }
@@ -51,17 +75,58 @@ async fn main() -> Result<(), std::io::Error> {
                     .expect("Expected that bytearray converts to u32");
 
                 let msg = Message::read_message(&mut _stream, msg_len).await;
-
-                _stream.write_all([0, 0, 0, 4].as_slice()).unwrap();
                 println!("MSG: {:?}", msg);
+
+                let resp_msg = Message::create_api_versions_response(
+                    msg.correlation_id,
+                    msg.request_api_key,
+                    msg.request_api_version,
+                );
+                println!("RESP: {:?}", resp_msg);
+
+                let mut resp_len = msg.correlation_id.to_be_bytes().len() as u32;
+                // error_code u16
+                resp_len += msg.error_code.to_be_bytes().len() as u32;
+                // Represents a sequence of objects of a given type T.
+                // Type T can be either a primitive type (e.g. STRING) or a structure.
+                // First, the length N is given as an INT32. Then N instances of type T follow.
+                // A null array is represented with a length of -1. In protocol documentation an array of T instances is referred to as [T].
+
+                // Length of [api_key]
+                resp_len += 3_u32.to_be_bytes().len() as u32;
+                // api_key - 18
+                resp_len += 18_u16.to_be_bytes().len() as u32;
+                // min_version 4+
+                resp_len += 4_u16.to_be_bytes().len() as u32;
+                resp_len += 4_u16.to_be_bytes().len() as u32;
+                // throttle_time_ms => INT32
+                resp_len += 0_u32.to_be_bytes().len() as u32;
+                println!("Response with msg len: {resp_len}");
+
+                /*
+                ApiVersions Response (Version: 4) => error_code [api_keys] throttle_time_ms TAG_BUFFER
+                error_code => INT16
+                api_keys => api_key min_version max_version TAG_BUFFER
+                  api_key => INT16
+                  min_version => INT16
+                  max_version => INT16
+                throttle_time_ms => INT32
+                */
+
+                _stream.write_all(&resp_len.to_be_bytes()).unwrap();
                 _stream
                     .write_all(&msg.correlation_id.to_be_bytes())
                     .unwrap();
-                if msg.request_api_version != 4 {
-                    println!("Unsupported api version: {}", &msg.request_api_version);
-                    _stream.write_all([0, 35].as_slice()).unwrap();
-                } else {
-                }
+                println!("Error code: {}", &msg.error_code);
+                _stream
+                    .write_all(&resp_msg.error_code.to_be_bytes())
+                    .unwrap();
+                //
+                _stream.write_all(&3_u32.to_be_bytes()).unwrap();
+                _stream.write_all(&18_u16.to_be_bytes()).unwrap();
+                _stream.write_all(&4_u16.to_be_bytes()).unwrap();
+                _stream.write_all(&4_u16.to_be_bytes()).unwrap();
+                _stream.write_all(&0_u32.to_be_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
